028447d3966050964ba5074edd0268e7ab34683c
Jiho Park <jiho5993@naver.com>
2021-05-16 19:37:11 +0900

    add scheduler comment

--- sample_EDF/EDF.c
+++ sample_EDF/EDF.c
@@ -32,7 +32,7 @@ void getProcesses()
 	}
 	fclose(InputFile);
 	// showing the processes;
-	printf("Pi| Ci, Di, pi\n");
+	printf("Pi | Ci, Di, pi\n");
 	printf("----------------\n");
 	for(i=0; i<processesNumber ; i++)
 	{
@@ -91,6 +91,7 @@ float schedulability()
 		y = ProcessesPeriode[i];
 		Condition = Condition + (x/y);
 		printf(" (%d/%d) ",ProcessesExecutionTime[i],ProcessesPeriode[i]);
+		if(i != processesNumber - 1) printf("+");
 	}
 	printf("is equal : %f", Condition);
 	return Condition;
@@ -98,12 +99,12 @@ float schedulability()
 
 void schedule()
 {
-	int earliestDeadline;
-	int earliestDeadlineIndex;
-	int schedulingTable[PeriodsLCM];
-	int remainsCapacity[1000];
-	int nextDeadline[1000];
-	int processOnlineNewPeriod[1000];
+	int earliestDeadline;					// 선택할 process의 deadline
+	int earliestDeadlineIndex;				// 선택할 process의 번호
+	// int schedulingTable[PeriodsLCM];
+	int remainsCapacity[1000];				// Pi의 남은 execution time
+	int nextDeadline[1000];					// Pi의 deadline
+	int processOnlineNewPeriod[1000];		// Pi의 지난 period time, period마다 burst하므로 항상 0부터 시작
 	for(i=0 ; i<processesNumber ; i++)
 	{
 		nextDeadline[i] = ProcessesDeadline[i];
@@ -114,71 +115,70 @@ void schedule()
 	FILE *OutputtFile;
 	OutputtFile = fopen("output.txt", "w");
 	fprintf(OutputtFile, "Periods LCM = %d\n",PeriodsLCM);
-	for(i=0; i<PeriodsLCM; i++)
+	for (i = 0; i < PeriodsLCM; i++)
 	{
-		printf("\n(%d,%d) : ",i,i+1);
-			
-			//getting the earliest deadline
-			earliestDeadline = PeriodsLCM;
-			earliestDeadlineIndex = -1;
-			for(j=0 ; j<processesNumber ; j++)
+		printf("\n(%d,%d) : ", i, i + 1);
+
+		//getting the earliest deadline
+		earliestDeadline = PeriodsLCM; // 가장 큰 값으로 LCM을 둔다
+		earliestDeadlineIndex = -1; // 현재 선택된 process가 없으므로 -1
+		for (j = 0; j < processesNumber; j++) // execute할 process 선택
+		{
+			if (remainsCapacity[j] > 0) // 남은 시간이 있다면
 			{
-				 if(remainsCapacity[j] > 0)
-				 {
-				 	
-				 	if(earliestDeadline > nextDeadline[j])
-				 	{
-				 		
-				 		earliestDeadline = nextDeadline[j];
-				 		earliestDeadlineIndex = j;
-					 }
-				 }
+
+				if (earliestDeadline > nextDeadline[j]) // earliest deadline first
+				{
+
+					earliestDeadline = nextDeadline[j];
+					earliestDeadlineIndex = j;
+				}
 			}
-			printf("     [exc = %d] ",earliestDeadlineIndex);
-			fprintf(OutputtFile, "(%d,%d) :   [exc = %d]\n",i,i+1,earliestDeadlineIndex);
-			remainsCapacity[earliestDeadlineIndex]--;
-			
-			//get the next deadline distance	
-			for(j=0 ; j<processesNumber ; j++)
+		}
+		printf("     [exc = %d] ", earliestDeadlineIndex);
+		fprintf(OutputtFile, "(%d,%d) :   [exc = %d]\n", i, i + 1, earliestDeadlineIndex);
+		remainsCapacity[earliestDeadlineIndex]--;
+
+		//get the next deadline distance
+		for (j = 0; j < processesNumber; j++)
+		{
+
+			if (processOnlineNewPeriod[j] == (ProcessesPeriode[j] - 1)) // 해당 프로세스의 주기가 지났다면 재할당
 			{
-				
-				 if(processOnlineNewPeriod[j] == (ProcessesPeriode[j] - 1 ) )
-				 {
-				 	nextDeadline[j] =  ProcessesDeadline[j];
-				 	remainsCapacity[j] = ProcessesExecutionTime[j];
-				 	processOnlineNewPeriod[j] = 0;
-				 }
-				 else
-				 {
-				 	if(nextDeadline[j] >0)
-				 	{
-				 		nextDeadline[j]--;
-					 }
-					 else
-					 {
-					 	if(remainsCapacity[j] > 0)
-					 	{
-					 		printf("\nthe process %d has no chance to complete its Capacity",j);
-					 		fprintf(OutputtFile, "\nthe process %d has no chance to complete its Capacity",j);
-						 }
-					 }
-				 	 processOnlineNewPeriod[j]++; 
-				 }
-				 
-				 
+				nextDeadline[j] = ProcessesDeadline[j];
+				remainsCapacity[j] = ProcessesExecutionTime[j];
+				processOnlineNewPeriod[j] = 0;
 			}
+			else // 아직 주기가 지나지 않았음
+			{
+				if (nextDeadline[j] > 0) // 1초가 지났으므로 deadline을 1초 줄임
+				{
+					nextDeadline[j]--;
+				}
+				else // deadline이 이미 지났다면
+				{
+					if (remainsCapacity[j] > 0) // deadline이 지났는데 아직 남은 execution time이 남았다면
+					{
+						printf("\nthe process %d has no chance to complete its Capacity", j);
+						fprintf(OutputtFile, "\nthe process %d has no chance to complete its Capacity", j);
+					}
+				}
+				processOnlineNewPeriod[j]++; // 주기에 대한 시간이 1초 지났음을 보여줌
+			}
+			printf("\nprocessOnlineNewPeriod[%d] : %d", j, processOnlineNewPeriod[j]);
+		}
 	}
 	fclose(OutputtFile);
 
 
 }
 
-main()
+int main()
 {
 	
 	
 	printf("EDF Programme \n");
-	for(i=0; i<1024000; i++);
+	// for(i=0; i<1024000; i++);
 	printf("done \n");
 	
 	
